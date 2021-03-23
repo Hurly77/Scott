94ed217962626f3a1b7d79cb48e1cd3b86bddad3
Cameron Leverett <camrbo@gmail.com>
2021-03-23 14:45:37 -0700

    removed code for better readability

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,32 +1,31 @@
+import {Fragment} from 'react'
+import theme from '../src/theme'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Btn from './UI/button'
 import {makeStyles} from '@material-ui/core/styles'
 
-const useStyles = makeStyles ({
-  root: {
-    backgroundColor: '#2eacab',
-    height: '100px'
-  } 
-})
+const useStyles = makeStyles (theme)
 const NavBar = () => {
   const classes = useStyles()
   return (
-    <div>
-      <AppBar position="static" size='lg' className={classes.root}>
+    <Fragment>
+      <AppBar position="relative" className={theme.primary}>
         <Toolbar>
-          <IconButton edge="start" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6">
-            Home
+          <Btn href={"/"} text={"Hello"} />
+        </Toolbar>
+      </AppBar>
+      <AppBar position="relative" className={classes.root}>
+        <Toolbar>
+          <MenuIcon fontSize="large" /> 
+          <Typography variant="h5" >
+            {`\u00A0 Menu`}
           </Typography>
         </Toolbar>
       </AppBar>
-    </div>
+    </Fragment>
   );
 }
 
